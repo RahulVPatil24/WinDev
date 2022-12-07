eac0ae2ddabd6430caab20dfa9e6ca05332fe4c9
RahulVPatil24 <rp98514360@gmail.com>
2022-12-07 10:31:32 +0530

    Add files via upload

--- 02-COM/04-Automation/02-Client/03-DotNetClient/01-CSharpClient/CSharpAutomation.cs
+++ 02-COM/04-Automation/02-Client/03-DotNetClient/01-CSharpClient/CSharpAutomation.cs
@@ -1,1 +1,24 @@
+using System;
+using System.Runtime.InteropServices;
+using AutomationServerTypeLibForDotNet;
 
+public class CSharpAutomation
+{
+    public static void Main()
+    {   
+        //code 
+        CMyMathClass objCMyMathClass = new CMyMathClass();
+        IMyMath objIMyMath  = (IMyMath)objCMyMathClass;
+
+        int num1, num2, sum, subtract;
+
+        num1 = 175;
+        num2 = 135;
+
+        sum = objIMyMath.SumOfTwoIntegers(num1, num2);
+        Console.WriteLine("Sum Of "+num1+" And "+num2+" is "+sum);
+
+        subtract = objIMyMath.SubtractionOfTwoIntegers(num1, num2);
+        Console.WriteLine("Sum Of "+num1+" And "+num2+" is "+subtract);
+    }
+}
