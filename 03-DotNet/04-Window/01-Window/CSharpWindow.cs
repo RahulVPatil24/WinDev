c95817ec91e265bc697fa207e3b2220b3dc277c5
RahulVPatil24 <rp98514360@gmail.com>
2022-12-07 11:25:45 +0530

    Add files via upload

--- 03-DotNet/04-Window/01-Window/CSharpWindow.cs
+++ 03-DotNet/04-Window/01-Window/CSharpWindow.cs
@@ -1,1 +1,18 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms; //For Windows UI Component
 
+public class CSharpWindow:Form
+{
+    public CSharpWindow()
+    {
+        Text = "CSharp Window"; //Text == caption CreateWindows's 3rd parameter in win32
+        BackColor = Color.Black; //hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH)
+        ResizeRedraw = true; //CS_HREDRAW | CS_VREDRAW;
+    }
+
+    public static void Main()
+    {
+        Application.Run(new CSharpWindow());
+    }
+}
