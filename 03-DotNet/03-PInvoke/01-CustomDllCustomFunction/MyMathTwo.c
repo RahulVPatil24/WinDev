4e717853b24f9fbb7e235d2712f488ba2d536aa9
RahulVPatil24 <rp98514360@gmail.com>
2022-12-07 10:36:36 +0530

    Add files via upload

--- 03-DotNet/03-PInvoke/01-CustomDllCustomFunction/MyMathTwo.c
+++ 03-DotNet/03-PInvoke/01-CustomDllCustomFunction/MyMathTwo.c
@@ -1,1 +1,27 @@
+//header files
+#include<windows.h>
 
+BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+	//Code
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		break;
+	case DLL_THREAD_ATTACH:
+
+		break;
+	case DLL_THREAD_DETACH:
+		break; 
+	case DLL_PROCESS_DETACH:
+		break;
+	default:
+		break;
+	}
+	return TRUE;
+}
+int MakeCube(int num)//microsoft specific specifier
+{
+	//code
+	return num * num * num;
+}
