d10144b49955aed6016153fdc132adb3d4ae5e8f
RahulVPatil24 <rp98514360@gmail.com>
2022-12-07 11:35:08 +0530

    Add files via upload

--- 03-DotNet/05-DotNetComInterop/02-DotNetServerComClient/DotNetServer.cs
+++ 03-DotNet/05-DotNetComInterop/02-DotNetServerComClient/DotNetServer.cs
@@ -1,1 +1,14 @@
+using System;
 
+public class DotNetServer
+{
+    public int SumOfTwoIntegers(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public int SubtractionOfTwoIntegers(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+}
