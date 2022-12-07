8092f0e57a7d13bc1ed49bef4c0048b6375bbb54
RahulVPatil24 <rp98514360@gmail.com>
2022-12-07 10:38:03 +0530

    Add files via upload

--- 03-DotNet/03-PInvoke/02-Win32DllWin32Function/01-Ansi/PInvoke.cs
+++ 03-DotNet/03-PInvoke/02-Win32DllWin32Function/01-Ansi/PInvoke.cs
@@ -1,1 +1,14 @@
+using System;
+using System.Runtime.InteropServices;
 
+public class PInvoke
+{
+
+    [DllImport("User32.dll")]    //attribute
+    public static extern int MessageBoxA(int Handle, String Message, String Caption, int Type);
+
+    public static void Main()
+    {
+        MessageBoxA(0, "This is Ansi MessageBox Via Interop", "Message", 0);
+    }
+}
