6a02a5e7174a8110e70eb13f83b3f585d6c504cd
RahulVPatil24 <rp98514360@gmail.com>
2022-12-07 11:30:09 +0530

    Add files via upload

--- 03-DotNet/04-Window/04-Icon/01-Externel/CSharpWindow.cs
+++ 03-DotNet/04-Window/04-Icon/01-Externel/CSharpWindow.cs
@@ -1,1 +1,46 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms; //For Windows UI Component
 
+public class CSharpWindow:Form
+{
+    public CSharpWindow()
+    {
+        Icon = new Icon("MyIcon.ico");
+        Text = "CSharp Window"; //Text == caption CreateWindows's 3rd parameter in win32
+        BackColor = Color.Black; //hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH)
+        ResizeRedraw = true; //CS_HREDRAW | CS_VREDRAW;
+        Width = 800;
+        Height = 600;
+
+        this.KeyDown += new KeyEventHandler(MyKeyDown);
+        this.MouseDown += new MouseEventHandler(MyMouseDown);   
+    }
+
+    protected override void OnPaint(PaintEventArgs pea)
+    {
+        Graphics grfx = pea.Graphics;
+
+        StringFormat strfmt = new StringFormat();
+
+        strfmt.Alignment = StringAlignment.Center;
+
+        strfmt.LineAlignment = StringAlignment.Center;
+        grfx.DrawString("Hello World !!!", Font, new SolidBrush(System.Drawing.Color.Cyan), ClientRectangle, strfmt);
+    }
+
+    void MyKeyDown(Object Sender, KeyEventArgs e)
+    {
+        MessageBox.Show("A Key Is Pressed");
+    }
+
+    void MyMouseDown(object Sender, MouseEventArgs e)
+    {
+        MessageBox.Show("Mouse is Clicked");
+    }
+
+    public static void Main()
+    {
+        Application.Run(new CSharpWindow());
+    }
+}
