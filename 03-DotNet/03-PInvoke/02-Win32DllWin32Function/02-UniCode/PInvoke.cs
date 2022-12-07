c1fe7b6f4e24c7e11e35d6a16d20526ee3c57a36
RahulVPatil24 <rp98514360@gmail.com>
2022-12-07 10:39:20 +0530

    Add files via upload

--- 03-DotNet/03-PInvoke/02-Win32DllWin32Function/02-UniCode/PInvoke.cs
+++ 03-DotNet/03-PInvoke/02-Win32DllWin32Function/02-UniCode/PInvoke.cs
@@ -1,1 +1,14 @@
+using System;
+using System.Runtime.InteropServices;
 
+public class PInvoke
+{
+
+    [DllImport("User32.dll",CharSet = CharSet.Unicode)]    //attribute
+    public static extern int MessageBoxW(int Handle, String Message, String Caption, int Type);
+
+    public static void Main()
+    {
+        MessageBoxW(0, "This is UniCode MessageBox Via Interop", "Message", 0);
+    }
+}
